edb713fac8e58a62a7ed3eb614ee6b7fc7129e30
Guasy12 <unai__13@hotmail.com>
2020-01-14 12:10:05 +0100

    ddl tipos alojamientos/ conexion BBDD

--- src/Index.aspx.vb
+++ src/Index.aspx.vb
@@ -1,8 +1,18 @@
-﻿Public Class Index
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Index
     Inherits System.Web.UI.Page
-    Public conector As String = ConfigurationManager.ConnectionStrings("myConnectionString").ConnectionString;
+    Public conector As String = ConfigurationManager.ConnectionStrings("myConnectionString").ConnectionString
     Public conexion As New MySqlConnection(conector)
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim query As New MySqlDataAdapter("SELECT DISTINCT lodgingtype FROM talojamientos ORDER BY lodgingtype ASC", conexion)
+        Dim campoTexto As New DataTable()
+        query.Fill(campoTexto)
+        Dim numero As Integer = campoTexto.Rows.Count
+        For i = 0 To campoTexto.Rows.Count - 1
+            ddlTipoAloj.Items.Add(campoTexto.Rows(i).Item(0))
+        Next
 
     End Sub
 
