04822d293cd173431568f6090b7aad4f8d6ce8e9
Guasy12 <unai__13@hotmail.com>
2020-01-14 09:42:31 +0100

    a

--- src/Index.aspx.vb
+++ src/Index.aspx.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Index
     Inherits System.Web.UI.Page
-
+    Public conector As String = ConfigurationManager.ConnectionStrings("myConnectionString").ConnectionString;
+    Public conexion As New MySqlConnection(conector)
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
