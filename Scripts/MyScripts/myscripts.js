40c500070fe61af2d7c70bd3f7a9dee780a2928e
Guasy12 <unai__13@hotmail.com>
2020-01-29 13:03:37 +0100

    fecha inicial y final

--- Scripts/MyScripts/myscripts.js
+++ Scripts/MyScripts/myscripts.js
@@ -1,10 +1,16 @@
-﻿$(function () {
+﻿
+$(function () {
     $('[id*=fechaInicio]').datepicker({
+        startDate: new Date(),
         changeMonth: true,
         changeYear: true,
         format: "dd/mm/yyyy",
         language: "es"
+    }).on('changeDate', function (selected) {
+        var minDate = new Date(selected.date.valueOf());
+        $('#fechaFin').datepicker('setStartDate', minDate);
     });
+
 });
 $(function () {
     $('[id*=fechaFin]').datepicker({
@@ -12,13 +18,18 @@ $(function () {
         changeYear: true,
         format: "dd/mm/yyyy",
         language: "es"
+    }).on('changeDate', function (selected) {
+        var maxDate = new Date(selected.date.valueOf());
+        $('#fechaInicio').datepicker('setEndDate', maxDate);
     });
 });
 
 $(function () {
     $('[id*=tbFechaNacimiento]').datepicker({
+        startDate: new Date('01/01/1990'),
         changeMonth: true,
         changeYear: true,
+        yearRange: "-100:+100",
         format: "dd/mm/yyyy",
         language: "es"
     });
